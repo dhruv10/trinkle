d93cbe1ca2feb916cf3f59531a193233f37f05e8
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2021-02-01 02:36:03 +0530

    refactor: 💡 created constants file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import App, {Container} from "next/app";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import withRedux from "next-redux-wrapper";
+
+import '../styles/globals.css';
+
+const reducer = (state = {foo: ''}, action) => {
+  switch (action.type) {
+      case 'FOO':
+          return {...state, foo: action.payload};
+      default:
+          return state
+  }
+};
+
+const makeStore = (initialState, options) => {
+  return createStore(reducer, initialState);
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
