b4f469518ad3e62502f864d912d90424a3ee057f
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2021-02-01 02:35:37 +0530

    style: 💄 statistics container

--- components/StatisticsContainer/index.js
+++ components/StatisticsContainer/index.js
@@ -1,6 +1,16 @@
 const StatisticsContainer = ({ styles, totalDistance }) => {
   return (
-    <div className={styles.container}>Total Distance: {totalDistance}</div>
+    <div className={styles.container}>
+      <p className={styles.title}>Trinkle</p>
+      <p className={styles.subtitle}>Distance Measurement App</p>
+      <p className={styles.distance}>
+        {(totalDistance * 1000).toFixed(2) || 0} m
+      </p>
+      <p className={styles.text}>Or we can say {totalDistance || 0} Km(s)</p>
+      <p className={styles.text}>
+        is the total distance between all selected points on the map!
+      </p>
+    </div>
   );
 };
 
