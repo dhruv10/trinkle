0e993231483ded714a5c6db0fa1cc5447b285821
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2021-01-31 23:29:11 +0530

    feat: 🎸 distance measurement working, able to console log

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import ReactMapGL, { Source, Layer } from 'react-map-gl';
 import length from '@turf/length';
 import Head from 'next/head';
 
@@ -7,12 +7,15 @@ import Head from 'next/head';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from '../styles/Home.module.css';
 
-const geojson = {
+const MAPBOX_TOKEN =
+  'pk.eyJ1IjoiZGhydXZiaGF0bmFnYXIxMCIsImEiOiJja2c3dmx6ZWowMmVmMnRtcWVkaWRlMjAzIn0.j_CTYHulY0ohTDABdHLr2g';
+
+const GEOJSON = {
   type: 'FeatureCollection',
   features: [],
 };
 
-const linestring = {
+const LINESTRING = {
   type: 'Feature',
   geometry: {
     type: 'LineString',
@@ -20,146 +23,138 @@ const linestring = {
   },
 };
 
-export default function Home() {
-  const [measuredDistance, setMeasuredDistance] = useState(undefined);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [viewport, setViewport] = useState({
-    width: '100vw',
-    height: '100vh',
-    latitude: 54.6872,
-    longitude: 25.2797,
-    zoom: 7,
+const measurePointLayer = {
+  id: 'measure-points',
+  type: 'circle',
+  source: 'geojson',
+  paint: {
+    'circle-radius': 5,
+    'circle-color': '#000',
+  },
+  filter: ['in', '$type', 'Point'],
+};
+
+const measureLineLayer = {
+  id: 'measure-lines',
+  type: 'line',
+  source: 'geojson',
+  layout: {
+    'line-cap': 'round',
+    'line-join': 'round',
+  },
+  paint: {
+    'line-color': '#000',
+    'line-width': 2.5,
+  },
+  filter: ['in', '$type', 'LineString'],
+};
+
+function App() {
+  const mapRef = React.useRef(null);
+
+  const [geojson, setGeojson] = React.useState(GEOJSON);
+  const [linestring, setLinestring] = React.useState(LINESTRING);
+  const [viewport, setViewport] = React.useState({
+    longitude: -122.45,
+    latitude: 37.78,
+    zoom: 14,
   });
 
-  const mapRef = useRef(null);
-
-  const onMapClick = (e) => {
-    const map = mapRef.current.getMap();
-    console.log('clicked', map)
-
-    const features = map.queryRenderedFeatures(e.point, {
+  const onMapClicked = (e) => {
+    let localGeojson = geojson;
+    let localLineString = linestring;
+    const features = mapRef.current.queryRenderedFeatures(e.point, {
       layers: ['measure-points'],
     });
 
-    if (geojson.features.length > 1) geojson.features.pop();
+    console.log(localGeojson);
+
+    if (localGeojson.features.length > 1) {
+      setGeojson((old) => ({
+        ...old,
+        features: old.features.pop(),
+      }));
+      //   localGeojson.features.pop();
+      console.log(localGeojson);
+    }
 
     if (features.length) {
       const id = features[0].properties.id;
-      geojson.features = geojson.features.filter(function (point) {
-        return point.properties.id !== id;
-      });
+      const tempFeatures = localGeojson.features.filter(
+        (point) => point.properties.id !== id
+      );
+
+      localGeojson.features = tempFeatures;
+
+      //   setGeojson((old) => ({
+      //     ...old,
+      //     features: tempFeatures,
+      //   }));
     } else {
       const point = {
         type: 'Feature',
         geometry: {
           type: 'Point',
-          coordinates: [e.lngLat.lng, e.lngLat.lat],
+          coordinates: [e.lngLat[0], e.lngLat[1]],
         },
         properties: {
           id: String(new Date().getTime()),
         },
       };
 
-      geojson.features.push(point);
+      //   setGeojson((old) => ({
+      //     ...old,
+      //     features: [...old.features, point],
+      //   }));
+      localGeojson.features.push(point);
     }
 
-    if (geojson.features.length > 1) {
-      linestring.geometry.coordinates = geojson.features.map(function (point) {
-        return point.geometry.coordinates;
-      });
+    if (localGeojson.features.length > 1) {
+      const tempCords = localGeojson.features.map(
+        (point) => point.geometry.coordinates
+      );
 
-      geojson.features.push(linestring);
-      const dist = length(linestring).toLocaleString();
+      localLineString.geometry.coordinates = tempCords;
 
-      // setMeasuredDistance(length(linestring).toLocaleString());
-      console.log(dist)
+      //   setLinestring((old) => ({
+      //     ...old,
+      //     geometry: {
+      //       ...old.geometry,
+      //       coordinates: tempCords,
+      //     },
+      //   }));
+
+      localGeojson.features.push(localLineString);
+
+      //   setGeojson((old) => ({
+      //     ...old,
+      //     features: [...old.features, linestring],
+      //   }));
+
+      console.log('Distance: ', length(localLineString).toLocaleString());
     }
 
-    map.getSource('geojson').setData(geojson);
+    // console.log(features);
+    setGeojson(localGeojson);
+    setLinestring(localLineString);
   };
-
-  const onMapLoad = () => {
-    console.log('LOADED');
-    const map = mapRef.current.getMap();
-
-    // map.addSource('geojson', {
-    //   type: 'geojson',
-    //   data: geojson,
-    // });
-
-    // map.addLayer({
-    //   id: 'measure-points',
-    //   type: 'circle',
-    //   source: 'geojson',
-    //   paint: {
-    //     'circle-radius': 5,
-    //     'circle-color': '#000',
-    //   },
-    //   filter: ['in', '$type', 'Point'],
-    // });
-
-    // map.addLayer({
-    //   id: 'measure-lines',
-    //   type: 'line',
-    //   source: 'geojson',
-    //   layout: {
-    //     'line-cap': 'round',
-    //     'line-join': 'round',
-    //   },
-    //   paint: {
-    //     'line-color': '#000',
-    //     'line-width': 2.5,
-    //   },
-    //   filter: ['in', '$type', 'LineString'],
-    // });
-
-    map.on('mousemove', onMapMouseMove)
-    map.on('click', onMapClick);
-  };
-
-  const onMapMouseMove = (e) => {
-    // console.log('mouse move', e)
-    const map = mapRef.current.getMap();
-    const features = map.queryRenderedFeatures(e.point, {
-      layers: ['measure-points'],
-    });
-
-    // UI indicator for clicking/hovering a point on the map
-    map.getCanvas().style.cursor = features.length ? 'pointer' : 'crosshair';
-  };
-
-  useEffect(() => {
-    const map = mapRef.current;
-    console.log(map)
-    onMapLoad()
-    if (isLoaded) {
-    }
-  }, [])
 
   return (
-    <div>
-      <Head>
-        <title>Distance Measurement App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-
-      <div className={styles.container}>
-        <h3>Accumulated Distance: {measuredDistance}</h3>
-      </div>
-
-      <ReactMapGL
-        {...viewport}
-        ref={mapRef}
-        // className={styles.map}
-        mapboxApiAccessToken='pk.eyJ1IjoiZGhydXZiaGF0bmFnYXIxMCIsImEiOiJja2c3dmx6ZWowMmVmMnRtcWVkaWRlMjAzIn0.j_CTYHulY0ohTDABdHLr2g'
-        mapStyle='mapbox://styles/mapbox/streets-v11'
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        onLoad={() => {
-          setIsLoaded(true);
-        }}
-        // onClick={onMapClick}
-        // onMouseMove={onMapMouseMove}
-      />
-    </div>
+    <ReactMapGL
+      ref={mapRef}
+      mapboxApiAccessToken={MAPBOX_TOKEN}
+      {...viewport}
+      width='100vw'
+      height='100vh'
+      onViewportChange={setViewport}
+      onClick={onMapClicked}
+    >
+      <Source id='geojson' type='geojson' data={geojson}>
+        <Layer {...measurePointLayer} />
+        <Layer {...measureLineLayer} />
+      </Source>
+    </ReactMapGL>
   );
 }
+
+export default App;
